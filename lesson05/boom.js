709f52f874329d0541930efc1be8577a42cfa698
kpcastillo <krn_iveth09@hotmail.com>
2023-02-02 09:59:20 -0800

    practice 05 done

---
+++ lesson05/boom.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const list = document.querySelector('#list')
+
+button.addEventListener('click', function() { 
+    const newItem = input.value
+    input.value = '';
+    
+
+    const liElement = document.createElement('li');
+
+    const deleteBtn = document.createElement('button');
+
+    liElement.textContent = newItem;
+    deleteBtn.textContent = 'X';
+
+    liElement.appendChild(deleteBtn);
+    list.appendChild(liElement);
+
+    deleteBtn.addEventListener('click', () => {
+        list.removeChild(liElement);
+    });
+
+    input.focus();
+    
+
+});
