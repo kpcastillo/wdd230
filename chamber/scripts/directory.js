9e9895489086b93d29682d984dfa7f5c99b17fc5
kpcastillo <krn_iveth09@hotmail.com>
2023-03-05 11:13:36 -0800

    tried to fix js

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,40 +1,40 @@
 const url = "https://kpcastillo.github.io/wdd230/chamber/json/data.json"
 
 const output = function(businessList){
-    businessList.forEach((business => {
-        let section = document.createElement('section')
-        let name = document.createElement('h3')
-        name.innerHTML = business.name
-        let description = document.createElement('p')
-        description.innerHTML = business.description
-        let address = document.createElement('h5')
-        address.innerHTML = business.address
-        let phonenumber = document.createElement('h5')
-        phonenumber.innerHTML = business.phonenumber
-        let website = document.createElement('p')
-        website.innerHTML = business.website
-        let membershiplevel = document.createElement('p')
-        membershiplevel.innerHTML = business.membershiplevel
+  businessList.forEach(business => {
+    let section = document.createElement('section')
+    let name = document.createElement('h3')
+    name.innerHTML = business.name
+    let description = document.createElement('p')
+    description.innerHTML = business.description
+    let address = document.createElement('h5')
+    address.innerHTML = business.address
+    let phonenumber = document.createElement('h5')
+    phonenumber.innerHTML = business.phonenumber
+    let website = document.createElement('p')
+    website.innerHTML = business.website
+    let membershiplevel = document.createElement('p')
+    membershiplevel.innerHTML = business.membershiplevel
 
-        portrait.setAttribute('src', business.imageurl);
-        portrait.setAttribute('alt', `Portait of ${business.name}`);
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '450');
+    portrait.setAttribute('src', business.imageurl);
+    portrait.setAttribute('alt', `Portait of ${business.name}`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '450');
 
-        section.appendChild(name);
-        section.appendChild(description)
-        section.appendChild(address)
-        section.appendChild(phonenumber);
-        section.appendChild(website)
-        section.appendChild(membershiplevel);
-        section.appendChild(portrait);
-        document.querySelector('#business-cards').append(section);
-    }))
+    section.appendChild(name);
+    section.appendChild(description)
+    section.appendChild(address)
+    section.appendChild(phonenumber);
+    section.appendChild(website)
+    section.appendChild(membershiplevel);
+    section.appendChild(portrait);
+    document.querySelector('#business-cards').append(section);
+    })
 }; 
 //output(businessList)
 
 async function getBusiness(url){
-    const response = await fetch(url)
+  const response = await fetch(url)
     if(response.ok){
       let businessData = await response.json()
       console.log('data', businessData)
@@ -48,3 +48,17 @@ async function getBusiness(url){
 }
 getBusiness(url);
 
+const grid = document.querySelector('#grid');
+const list = document.querySelector('#list');
+
+const listDisplay = function(){
+  display.classList.add('list')
+  display.classList.remove('grid')
+}
+list.addEventListener('click', listDisplay)
+
+const gridDisplay = function(){
+  display.classList.add('grid')
+  display.classList.remove('list')
+}
+grid.addEventListener('click', gridDisplay)
