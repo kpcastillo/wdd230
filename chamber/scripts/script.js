bf2bfbf49bb2441316d227611dbd102882711fe0
kpcastillo <krn_iveth09@hotmail.com>
2023-03-07 16:57:03 -0800

    edited script

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,3 +1,4 @@
+//Hamburger menu
 function toggleMenu(){
     document.getElementById('navMenu').classList.toggle('open');
     document.getElementById('btn').classList.toggle('open');
@@ -6,6 +7,7 @@ function toggleMenu(){
 const menu = document.getElementById('btn');
 menu.onclick = toggleMenu;
 
+//Display current date
 const datefield = document.querySelector(".date");
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
@@ -13,20 +15,22 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 );
 datefield.innerHTML = `<strong>${fulldate}</strong>`;
 
+//Last modified
 let LastModif = new Date(document.lastModified);
 document.querySelector('#last-mod').textContent = LastModif;
 
-
-
+//Display banner with a message
 let dayOfWeek = now.getDay();
 if(dayOfWeek ===  1 || dayOfWeek === 2 ){
-    let message = 'Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
+    message = 'Come join us for the chamber meet and greet Wednesday at 7:00 p.m.'
     console.log(message);
-    document.querySelector('#banner').innerHTML = message
+   
 }
+let banner = document.querySelector('#banner')
+banner = message
  
 
-
+//Lazy load images
 let loadingImg = document.querySelectorAll('img[data-src]');
 
 const loadImages = (image) => {
@@ -52,4 +56,38 @@ else {
     loadingImg.forEach((img) => {
       loadImages(img);
     });
+}
+
+//Display number of visits using local storage
+const visitsDisplay = document.querySelector("#visits");
+
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+numVisits++;
+localStorage.setItem("visits-ls", numVisits);
+
+
+//Display date of last visit
+const lastVisit = document.querySelector("#last-visit");
+
+let visit = window.localStorage.getItem("visits-ls")
+
+let lastVisitDay = {visit, timestamp: new Date()}
+console.log(lastVisitDay, "Last visot date")
+lastVisit.innerHTML = lastVisitDay.timestamp
+
+//Display time in days from last visit
+const daysFromLastVisit = document.querySelector("#days");
+const days = now - lastVisitDay.timestamp
+
+if (days <= 0) {
+	daysFromLastVisit.innerHTML = `0`;
+} else {
+  daysFromLastVisit.innerHTML = days;
 }
