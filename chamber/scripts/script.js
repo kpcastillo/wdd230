ac5cc55bf8c14ae525b51ce3d63dec716b880c16
kpcastillo <krn_iveth09@hotmail.com>
2023-03-21 21:00:16 -0700

    attempt to display dynamic card

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -60,11 +60,11 @@ const visitsDisplay = document.querySelector("#visits");
 
 let numVisits = Number(window.localStorage.getItem("visits-ls"));
 
-if (numVisits !== 0) {
+if (numVisits != 0) {
 	visitsDisplay.textContent = numVisits;
 }
 else {
-	visitsDisplay.textContent = `This is your first visit!`;
+	visitsDisplay.textContent = 'This is your first visit!';
 }
 
 numVisits++;
---
+++ chamber/scripts/spotlight.js
@@ -0,0 +1,46 @@
+const output = function(business){
+      let section = document.createElement('section')
+      let name = document.createElement('h3')
+      name.innerHTML = business.name
+      let phonenumber = document.createElement('h5')
+      phonenumber.innerHTML = business.phonenumber
+      let website = document.createElement('p')
+      website.innerHTML = business.website
+      let logo = document.createElement('img');
+  
+      logo.setAttribute('src', business.image);
+      logo.setAttribute('alt', `Portait of ${business.name}`);
+      logo.setAttribute('loading', 'lazy');
+      logo.setAttribute('width', '250');
+  
+      section.appendChild(name);
+      section.appendChild(phonenumber);
+      section.appendChild(website)
+      section.appendChild(logo);
+      document.querySelector('#dynamic-card').append(section);
+}; 
+  
+async function getBusiness(url){
+    const response = await fetch(url)
+      if(response.ok){
+        let businessData = await response.json()
+        businessList = businessData
+    }
+    
+    
+}
+
+const get_random = function(businessList){
+    const randomList = []
+    businessList.companies.forEach(business => {
+        if (business.membership == "Gold"){
+            randomList.push(business)
+            const random = randomList[Math.floor(Math.random() * randomList.length)];
+            output(random)
+        }
+    })
+};
+const url = "https://kpcastillo.github.io/wdd230/chamber/json/data.json"
+const businessList = getBusiness(url)
+get_random(businessList)
+    
