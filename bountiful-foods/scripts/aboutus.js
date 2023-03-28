bf880639505fca21fa168bc4724c22336b4e8566
kpcastillo <krn_iveth09@hotmail.com>
2023-03-28 11:39:44 -0700

    weather card added

--- bountiful-foods/scripts/aboutus.js
+++ bountiful-foods/scripts/aboutus.js
@@ -1,3 +1,12 @@
 //Last modified
 let modDate = new Date (document.lastModified)
 document.querySelector('#last-modified').textContent = `Last Modified on: ${modDate}`
+
+//Nav
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+  
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
--- bountiful-foods/scripts/script.js
+++ bountiful-foods/scripts/script.js
@@ -34,3 +34,30 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = 'block';  
   dots[slideIndex-1].className += 'active';
 }
+//Weather Card
+url = 'https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&units=imperial&appid=55079db1c440a3aa70df7a9d3c6266fb'
+
+let currentTemp= document.querySelector('#current-temp');
+let currentConditions = document.querySelector('#current-cond')
+let humidity = document.querySelector('#humidity')
+let forecast = document.querySelector('#forecast')
+
+async function weatherFetch(){
+  const response = await fetch(url)
+  if (response.ok){
+    const data = await response.json()
+    console.log(data, 'Data')
+    weatherCard(data)
+  }
+  else{
+    throw Error(await response.text());
+  }
+
+}
+function weatherCard(data){
+  currentTemp.innerHTML = data.main.temp.toFixed(0)
+  currentConditions.innerHTML = data.weather[0].description
+  humidity.innerHTML = data.main.humidity
+
+}
+weatherFetch()
