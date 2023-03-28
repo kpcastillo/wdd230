3d6adac19bb29848f298cecf1ae5a86f132b7348
kpcastillo <krn_iveth09@hotmail.com>
2023-03-28 09:49:39 -0700

    edited about us page

---
+++ bountiful-foods/scripts/aboutus.js
@@ -0,0 +1,3 @@
+//Last modified
+let modDate = new Date (document.lastModified)
+document.querySelector('#last-modified').textContent = `Last Modified on: ${modDate}`
--- bountiful-foods/scripts/script.js
+++ bountiful-foods/scripts/script.js
@@ -31,10 +31,6 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex-1].style.display = 'block';  
+  dots[slideIndex-1].className += 'active';
 }
-
-//Last modified
-let modDate = new Date (document.lastModified)
-document.querySelector('#last-modified').textContent = modDate
